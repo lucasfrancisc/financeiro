e73c411bd6be5a32bbdf6db664f4e03d042e717e
Lucas Francisco <lucasfrancisco.sistemas@outlook.com>
2019-01-08 20:04:52 -0200

    Impressão do relatório de títulos.

--- src/controller/TituloController.java
+++ src/controller/TituloController.java
@@ -1,16 +1,28 @@
 package controller;
 
+import java.io.File;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
 import model.dao.database.jpa.TituloDAO;
 import model.entity.Titulo;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import util.enumeration.Operacao;
 
 @Resource
 public class TituloController extends GenericController<Titulo, TituloDAO> implements GenericInterfaceController<Titulo>{
 	
-	public TituloController(Result result) {
+	private final ServletContext context;
+	
+	public TituloController(Result result, ServletContext context) {
 		super(result);
+		this.context = context;
 	}
 	
 	public void add() {
@@ -35,5 +47,21 @@ public class TituloController extends GenericController<Titulo, TituloDAO> imple
 	
 	public void listagem(String pesquisa) {
 		super.listagem(pesquisa);
+	}
+	
+	public byte[] relatorio() {
+		String path = "relatorios" + File.separator + "titulos.jasper";
+		String caminhoJasper = context.getRealPath(path);
+		
+		List<Titulo> list = dao.find("");
+		try {
+			JasperPrint impressao = JasperFillManager.fillReport(caminhoJasper, null, new JRBeanCollectionDataSource(list)); 			
+			byte[] pdf = JasperExportManager.exportReportToPdf(impressao);			
+			return pdf;
+		} catch (Exception e) {
+			System.err.println(e);
+		}
+		
+		return null;		
 	}
 }
